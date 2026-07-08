9b8a0225774917ed41050cba5a87ec6e5f5bac57
marknikolaev32036-ux <marknikolaev32036@gmail.com>
2026-07-08 12:35:47 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,6 @@
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
-
 export default defineConfig(() => {
   return {
-    base: '/index.html/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
